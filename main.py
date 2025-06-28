96d4188ef030e532846b9002cc1653b917990de5
x-AZMAN-x <fromrimu@gmail.com>
2025-06-28 13:08:12 +0600

    my first python project

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+Person1
+"""
+person1 = "Azman"
+age1 = 12
+is_student1 = True
+"""
+Person2
+"""
+person2 = "Samin"
+age2 = 12.8
+is_student2 = True
+"""
+Info About Person1
+"""
+print(f"Data Type Of {person1}:{type(person1)}. The Data Type Of {age1}: {type(age1)}. And Person1 Is A Student And Its Type => {type(is_student1)}.")
+"""
+Info About Person2
+"""
+print(f"Data Type Of {person2}:{type(person2)}. The Data Type Of {age2}: {type(age2)}. And Person2 Is A Student And Its Type => {type(is_student2)}.")
